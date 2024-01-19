8b1869f610a415f68bd853a5425e196445cdaeaa
KowshikanN <kowshikanWork@gmail.com>
2024-01-19 02:13:43 -0500

    Added comment

--- keysTemplate.py
+++ keysTemplate.py
@@ -1,4 +1,5 @@
 #Template for adding API keys.
+#*Change name of file to keys.py*
 BOTTOKEN = 'DISCORD BOT API KEY GOES HERE!'
 GPTKEY = 'CHATGPT (First test key) API KEY GOES HERE!'
 GPTKEY2 = 'CHATGPT (Real key) API KEY GOES HERE!'
